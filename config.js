16dca766e408aae1aff3dae31113e4c2acff741d
Bluffknight <dchisholm96@gmail.com>
2024-10-27 05:19:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "lavalink01.techbyte.host",
-        port: 2036,
-        password: "NAIGLAVA-dash.techbyte.host",
+        host: "LavaLink4.lightsout.in",
+        port: 40069,
+        password: "LightsoutOwnsElves",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
