ee913970e4e760c0ae68c1c7bc7fd3cca79bd170
Bluffknight <dchisholm96@gmail.com>
2024-11-23 13:35:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "lava.inzeworld.com",
-        port: 3128,
-        password: "saher.inzeworld.com",
+        host: "212.227.65.132",
+        port: 10296,
+        password: "charlesnaig-lavalink",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
